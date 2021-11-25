d56594b2b7d0047982909a328ee36f6c91fcb151
ynguyen <ndty14@gmail.com>
2021-11-26 01:38:53 +0700

    flask

---
+++ flaskBlog/flaskblog/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '69c674200e0406c9db0b66ba17f1cc8e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+from flaskblog import routes
---
+++ flaskBlog/run.py
@@ -0,0 +1,6 @@
+from flaskblog import app
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
